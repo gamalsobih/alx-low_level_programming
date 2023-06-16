6d55481729bed38d95926d8f1cd5065a11b970fc
gamalsobih <gamalsob@gmail.com>
2023-06-15 18:29:17 -0700

    Starting to code today, so cool

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * return: 0 (sucsses)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
