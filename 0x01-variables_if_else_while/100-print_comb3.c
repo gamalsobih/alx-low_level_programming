4bcf1e9daf0fb84b0b5cd2603c09a85285ecc465
gamalsobih <gamalsob@gmail.com>
2023-06-18 15:49:12 -0700

    Starting to code today, so cool

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int n = 1 + i;
+	char m[10] = "0123456789";
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n ; n < 10; n++)
+		{
+		putchar(m[i]);
+		putchar(m[n]);
+	};
+		n = 0;
+
+	}
+	putchar('\n');
+	return (0);
+}
