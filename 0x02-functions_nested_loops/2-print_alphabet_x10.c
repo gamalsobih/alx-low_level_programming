c76e106e08be78600a727f26f81a38ea7bc707bd
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-06-21 02:53:03 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int i ,n;
-for (n =0; n < 10 , n++){
-	for (i = 97; i <= 122; i++)
+	int i, n;
+for (n =0; n < 10 , n++)
 	{
-		_putchar(i);
-	}
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
 	_putchar('\n');
-}
-	return(0);
+	}
 }
 
