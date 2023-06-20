26365cfa930bb2585a4129e6feffc0a23fbfae39
gamalsobih <gamalsob@gmail.com>
2023-06-20 16:48:58 -0700

    Starting to code today, so cool

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+for (int n =0; n < 10 , n++){
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}}
+
