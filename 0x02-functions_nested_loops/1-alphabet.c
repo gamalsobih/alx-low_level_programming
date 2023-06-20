b57e53e998b2a6acdd3d9ad4bdffba1e60a23784
gamalsobih <gamalsob@gmail.com>
2023-06-20 16:40:43 -0700

    Starting to code today, so cool

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints main as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	
+	int count;
+
+	
+	for (count = 97; count < 123; count++)
+	{
+		_putchar(count);
+	}
+	_putchar('\n');
+	return (0);
+}
