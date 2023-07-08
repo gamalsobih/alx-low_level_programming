047abbe20ee8209b4c219cc21cd6400f576fe19a
gamalsobih <gamalsob@gmail.com>
2023-07-07 18:55:10 -0700

    Starting to code today, so cool

---
+++ 0x09-static_libraries/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int n;
+	int  m[10] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = i + 1 ; n < 10; n++)
+		{
+		putchar(m[i]);
+		putchar(m[n]);
+		if (i < 8)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	};
+	putchar('\n');
+	return (0);
+}
---
+++ 0x09-static_libraries/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int n;
+	int g;
+	int  m[10] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
+
+	for (i = 0; i < 10; i++)
+	{
+		for (g = i + 1; g < 10 ; g++)
+		{
+			for (n = g + 1 ; n < 10; n++)
+			{
+			putchar(m[i]);
+			putchar(m[g]);
+			putchar(m[n]);
+			if (i < 7)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			}
+		}
+
+	};
+	putchar('\n');
+	return (0);
+}
