d0aa784a04bde3f92dd349a914d6ba52986cf42f
gamalsobih <gamalsob@gmail.com>
2023-07-19 16:56:42 -0700

    Starting to code today, so cool

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+*    main = print f
+*       returne 0
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    if (array && size && action)
+    {
+            for(int i = 0 ; i<size ; i++)
+            {
+                    action(array[i])
+            }
+    }
+}
+
+
