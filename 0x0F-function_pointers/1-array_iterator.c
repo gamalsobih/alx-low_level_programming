390589eb876b0c6098dc6c5df04bb73ebbb81708
gamalsobih <gamalsob@gmail.com>
2023-07-19 16:58:47 -0700

    Starting to code today, so cool

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,7 +6,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    if (array && size && action)
+    if (array && action)
     {
             for(int i = 0 ; i<size ; i++)
             {
