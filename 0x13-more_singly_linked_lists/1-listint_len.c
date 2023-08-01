bcc4654cf1e1153744cad2c4e9b08fc4c2c565c3
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-01 20:03:24 +0300

    Update 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+    size_t gnode = 0;
+    while (h !=NULL)
+    {
+        h = h->next;
+        gnode++;
+    }
+    printf ("%d\n",gnode);
+    return(gnode);
+
+}
