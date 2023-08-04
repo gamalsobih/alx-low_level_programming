72411b61fe0420e2bf27389549dcec0c7943c1af
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-05 00:49:15 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 /**
-define this is a print list function
-returne te returne is a x
-     char *str;
-    unsigned int len;
-    struct list_s *next;
-*/
-size_t list_len(const list_t *h){
-    size_t x = 0;
-    while (h != NULL)
-    {
-        h=h->next;
-        x++;
-    }
-    return(x);
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nc;
+
+	nelem = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		nc++;
+	}
+	return (nc);
 }
-
-
