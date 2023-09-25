335a221e98e28cded84b90dcb861df9b08a11253
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-25 20:24:58 +0300

    Add files via upload

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add a new node at the beginning
+ * of a linked list
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NUll if it failed.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *nw;
+
+	nw = malloc(sizeof(listint_t));
+
+	if (nw == NULL)
+		return (NULL);
+
+	nw->n = n;
+	nw->next = *head;
+	*head = nw;
+
+	return (*head);
+}
